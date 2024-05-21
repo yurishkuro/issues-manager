5ae2c24e04a110c455b66b68d89eafa8c2417e42
Yuri Shkuro <github@ysh.us>
2024-05-21 00:53:52 -0400

    format
    Signed-off-by: Yuri Shkuro <github@ysh.us>

--- src/config.ts
+++ src/config.ts
@@ -3,32 +3,32 @@
  */
 
 interface State {
-  description: string;
-  label: string;
-  transitions: Transition[];
+  description: string
+  label: string
+  transitions: Transition[]
 }
 
 interface Transition {
-  description: string;
-  conditions?: Condition[];
-  actions?: Action[];
+  description: string
+  conditions?: Condition[]
+  actions?: Action[]
 }
 
 interface Condition {
-  type: 'label' | 'timeout' | 'activity' | 'pull-request';
-  label?: string; // for type=label
-  timeout?: number; // in days, for type=timeout
-  linked?: boolean; // for type=pull-request
+  type: 'label' | 'timeout' | 'activity' | 'pull-request'
+  label?: string // for type=label
+  timeout?: number // in days, for type=timeout
+  linked?: boolean // for type=pull-request
 }
 
 interface Action {
-  type: 'add-label' | 'replace-label' | 'remove-label' | 'post-comment';
-  label?: string;
-  comment?: string;
+  type: 'add-label' | 'replace-label' | 'remove-label' | 'post-comment'
+  label?: string
+  comment?: string
 }
 
 interface StateMachineConfig {
-  states: State[];
+  states: State[]
 }
 
-export { State, Transition, Condition, Action, StateMachineConfig };
+export { State, Transition, Condition, Action, StateMachineConfig }
